4e04517e247935358eec10ddb06b4af6cd977381
zhuazima <1025854946@qq.com>
2021-11-20 20:17:26 +0800

    add project files

--- hal/hal_time.c
+++ hal/hal_time.c
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-11-20 19:28:49
+ * @LastEditTime: 2021-11-20 20:17:15
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \Wifi Host\hal\hal_time.c
+ */
 #include "stm32f10x.h"
 #include "hal_led.h"
 #include "hal_time.h"
@@ -40,9 +48,14 @@ static void hal_timer4Config(void)
 	NVIC_Init(&NVIC_InitStructure);
 }
 
+
+
+
 void TIM4_IRQHandler(void)
 {
 	TIM_ClearFlag(TIM4, TIM_FLAG_Update);
 	 
 	
 }
+
+
